2dc9d0f8998502f4201a75f5f719a8d1e47fa801
peterthys <63521795+peterthys@users.noreply.github.com>
2020-05-09 18:38:21 +0200

    buttons in fragmant_main geactiveerd

--- app/src/main/java/com/example/werk/MainFragment.kt
+++ app/src/main/java/com/example/werk/MainFragment.kt
@@ -5,16 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_main.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class MainFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //TODO : als customer popupmenu geworden is moet hier "nieuwe klant"uit dat menu komen
+        bt_customer.setOnClickListener {
+            view.findNavController().navigate(R.id.action_mainFragment_to_newCustomerFragment)
+
+        }
+        bt_overview.setOnClickListener {
+            view.findNavController().navigate(R.id.action_mainFragment_to_overviewFragment)
+
+        }
+        bt_add.setOnClickListener {
+            view.findNavController().navigate(R.id.action_mainFragment_to_overviewFragment)
+        }
+    }
 }
