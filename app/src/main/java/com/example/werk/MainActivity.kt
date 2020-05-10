0f05538486474c8cbaf1d9a64c192a776bda91d7
peterthys <63521795+peterthys@users.noreply.github.com>
2020-05-10 11:53:47 +0200

    spinner werkt

--- app/src/main/java/com/example/werk/MainActivity.kt
+++ app/src/main/java/com/example/werk/MainActivity.kt
@@ -13,24 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var customersArray = arrayOf("Customer 1","Customer 2","Customer 3","Customer 4")
-        val arrayAdapter = ArrayAdapter(this,android.R.layout.simple_spinner_item, customersArray )
 
-        sp_spinner.adapter = arrayAdapter
-        sp_spinner.onItemSelectedListener = object :
-
-
-            AdapterView.OnItemSelectedListener{
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                tv_customer.text= customersArray[position]
-
-            }
-
-        }
     }
 }
 
--- app/src/main/java/com/example/werk/MainFragment.kt
+++ app/src/main/java/com/example/werk/MainFragment.kt
@@ -1,12 +1,16 @@
 package com.example.werk
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.ticket_overview.*
 
 /**
  * A simple [Fragment] subclass.
@@ -23,6 +27,21 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        var customersArray = arrayOf("Customer 1","Customer 2","Customer 3","Customer 4")
+        val arrayAdapter = ArrayAdapter(requireContext(),android.R.layout.simple_spinner_item, customersArray )
+
+        sp_spinner.adapter = arrayAdapter
+        sp_spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                //tv_customer.text= customersArray[position]
+
+            }
+        }
 
         bt_customer.setOnClickListener {
             view.findNavController().navigate(R.id.action_mainFragment_to_newCustomerFragment)
