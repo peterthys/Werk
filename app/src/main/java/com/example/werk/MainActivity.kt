24f4f63c99424d4d29db93c094f2d968f41f7eb2
peterthys <63521795+peterthys@users.noreply.github.com>
2020-05-10 16:05:10 +0200

    class Customer toegevoegd

--- app/src/main/java/com/example/werk/MainActivity.kt
+++ app/src/main/java/com/example/werk/MainActivity.kt
@@ -5,14 +5,29 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import com.example.werk.klassen.Customer
 import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.android.synthetic.main.ticket_overview.*
 
 class MainActivity : AppCompatActivity() {
+    var listCustomers = ArrayList<Customer>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        listCustomers.add(
+            Customer(
+                1,
+                "Ceulemans Peter",
+                "Aannemer",
+                " 0476418746 ",
+                "peterceulemans.bvba@skynet.be",
+                "Schriekstraat 140 2223 Schriek ",
+                "",
+                "",
+                "BE0860 733 953 "
+            )
+        )
 
     }
 }
--- app/src/main/java/com/example/werk/klassen/Customer.kt
+++ app/src/main/java/com/example/werk/klassen/Customer.kt
@@ -1,4 +1,27 @@
 package com.example.werk.klassen
 
 class Customer {
+    var customerID : Int ? = null
+    var customerName : String ? = null
+    var customerDesecription : String ?= null
+    var customerPhone : String ? = null
+    var customerEmail : String ? = null
+    var customerAdress1 : String ?= null
+    var customerAdress2 : String ?= null
+    var customerInfo : String ?= null
+    var customerBTWnr : String ?= null
+
+    constructor(customerID : Int, customerName :String, customerDesecription : String,customerPhone : String, customerEmail :String,customerAdress1 : String,
+                customerAdress2 : String,customerInfo : String ,customerBTWnr : String){
+        this.customerID = customerID
+        this.customerName = customerName
+        this.customerDesecription = customerDesecription
+        this.customerPhone = customerPhone
+        this.customerEmail = customerEmail
+        this.customerAdress1 = customerAdress1
+        this.customerAdress2 = customerAdress2
+        this.customerInfo = customerInfo
+        this.customerBTWnr = customerBTWnr
+    }
+
 }
