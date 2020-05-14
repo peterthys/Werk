85fd10a82f5532633de71711356b34b0a91014df
peterthys <63521795+peterthys@users.noreply.github.com>
2020-05-15 00:10:16 +0200

    repository aangepast

--- app/src/main/java/com/example/werk/klassen/JobPerformanceRepository.kt
+++ app/src/main/java/com/example/werk/klassen/JobPerformanceRepository.kt
@@ -1,17 +1,24 @@
 package com.example.werk.klassen
 
+import android.app.ListActivity
 import androidx.lifecycle.LiveData
+import com.example.werk.database.CustomersDao
 import com.example.werk.database.JobPerformanceDao
 
 // Declares the DAO as a private property in the constructor. Pass in the DAO
 // instead of the whole database, because you only need access to the DAO
-class JobPerformanceRepository(private val jobPerformanceDao: JobPerformanceDao) {
+class JobPerformanceRepository(private val jobPerformanceDao: JobPerformanceDao,private val customersDao: CustomersDao) {
 
     // Room executes all queries on a separate thread.
     // Observed LiveData will notify the observer when the data has changed.
     val allJobPerformances: LiveData<List<JobPerformance>> = jobPerformanceDao.getAll()
+    val allCustomers: LiveData<List<Customer>> = customersDao.getAll()
 
     suspend fun insert(jobPerformance: JobPerformance) {
         jobPerformanceDao.insert(jobPerformance)
     }
+    suspend fun insertCustomer(customer: Customer) {
+        customersDao.insert(customer)
+    }
+
 }
